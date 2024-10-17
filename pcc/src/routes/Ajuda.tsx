7510a0b1f68d89eeff596977e84869cd419f153c
gusandrini <gustavosandrini92@gmail.com>
2024-10-17 17:09:35 -0300

    adicionado rota da pagina de ajuda

---
+++ pcc/src/routes/Ajuda.tsx
@@ -0,0 +1,48 @@
+import Cabecalho from '@/components/Cabecalho/Cabecalho';
+import Rodape from '@/components/Rodape/Rodape';
+import Link from 'next/link';
+
+
+const Ajuda = () => {
+    return (
+        <div>
+            <Cabecalho/>
+
+            <div className="paginas">
+                <Link href="/"> Home / Ajuda</Link>
+            </div>
+
+            <div className="contato">
+                <section className="stato">
+                    <form className="ftato">
+                        <div className="center">
+                            <label className="titulo"> ÁREA DE CONTATO:</label>
+                        </div>
+                        <div>
+                            <label></label>
+                            <input type="text" name="name" placeholder="Digite seu nome" required />
+                        </div>
+
+                        <div>
+                            <label></label>
+                            <input type="email" name="email" placeholder="Digite seu email" required />
+                        </div>
+
+                        <div>
+                            <label></label>
+                            <textarea name="message" rows={30} placeholder="Digite sua mensagem" required></textarea>
+                        </div>
+
+                        <div>
+                            <input className="center" type="submit" value="Enviar" />
+                        </div>
+                    </form>
+                </section>
+            </div>
+
+            <Rodape/>
+        </div>
+    );
+};
+
+export default Ajuda
