e05c149f9c0443bfebb7134c02f5148d1c180017
gusandrini <gustavosandrini92@gmail.com>
2024-10-18 08:07:47 -0300

    corrigindo caminho das páginas

--- pcc/src/components/Cabecalho/Cabecalho.tsx
+++ pcc/src/components/Cabecalho/Cabecalho.tsx
@@ -11,7 +11,7 @@ function Cabecalho() {
                 <nav>
                     <a href="#"><Image src={Porto} alt="logo da Porto Seguro" className='logo-porto' /></a>
                     <ul>
-                        <li><Link href="/sobre">SOBRE NÓS</Link></li>
+                        <li><Link href="">SOBRE NÓS</Link></li>
                         <li><Link href="/ajuda">AJUDA</Link></li>
                         <li><a className="watson" href="https://web-chat.global.assistant.watson.appdomain.cloud/preview.html?region=us-south&integrationID=250f2872-90d1-4b69-a463-e7c03c0581c9&serviceInstanceID=cd14777c-39ac-401b-938f-9849d2bb018f">
                             ASSISTENTE VIRTUAL</a></li>
