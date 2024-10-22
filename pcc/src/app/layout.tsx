6fe3d1d28c9f2c72a08ec217b894d49b9406d344
gusandrini <gustavosandrini92@gmail.com>
2024-10-22 19:27:55 -0300

    tentado adicionar fonte

--- pcc/src/app/layout.tsx
+++ pcc/src/app/layout.tsx
@@ -2,7 +2,14 @@ import Cabecalho from "@/components/Cabecalho/Cabecalho";
 import Rodape from "@/components/Rodape/Rodape";
 import type { Metadata } from "next";
 import "./globals.css";
+import {Inter} from 'next/font/google'
+// import Head from "next/head";
 
+const mainFontFamily = Inter({
+  weight: ['300', '400', '700'],
+  subsets: ['latin'],
+  variable: '--font-family-main'
+});
 
 export const metadata: Metadata = {
   title: "PortoCarCare",
@@ -15,8 +22,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br">
-      <body>
+    <html lang="pt-br" className={mainFontFamily.variable}>
+      <body >
         <Cabecalho />
         {children}
         <Rodape />
--- pcc/tailwind.config.ts
+++ pcc/tailwind.config.ts
@@ -8,6 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily:{
+        sans: ['var(--font-family-main)']
+
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
