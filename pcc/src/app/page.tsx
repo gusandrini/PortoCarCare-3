62b68a33774ae84439ace4594f5303f1e229561d
gusandrini <gustavosandrini92@gmail.com>
2024-10-28 11:06:38 -0300

    ajuste estilização

--- pcc/src/app/page.tsx
+++ pcc/src/app/page.tsx
@@ -10,10 +10,10 @@ export default function Home() {
                 <h1>
                     Conte com um seguro de carro, conheça a Seguro Auto da Porto Seguro!
                 </h1>
-                <Link 
-                    className='saiba-mais' 
-                    href="https://www.portoseguro.com.br/seguro-auto?utm_source=google&utm_medium=cpa&utm_campaign=automovel_na_marca_search_aon_performance_hq_growth_auto&utm_content=key_transacao_automatico_cross_marca_pesquisa_saiba-mais_nu_teste-nao_24uzq00229he&ref=24uzq00229he&gad_source=1&gclid=CjwKCAjw59q2BhBOEiwAKc0ije0oTEJb6IhSgK-USQHB-55pAB6qF13TNSn_obiCI7hn7Xv5KRYsKxoCiVQQAvD_BwE" 
-                    target="_blank" 
+                <Link
+                    className='saiba-mais'
+                    href="https://www.portoseguro.com.br/seguro-auto?utm_source=google&utm_medium=cpa&utm_campaign=automovel_na_marca_search_aon_performance_hq_growth_auto&utm_content=key_transacao_automatico_cross_marca_pesquisa_saiba-mais_nu_teste-nao_24uzq00229he&ref=24uzq00229he&gad_source=1&gclid=CjwKCAjw59q2BhBOEiwAKc0ije0oTEJb6IhSgK-USQHB-55pAB6qF13TNSn_obiCI7hn7Xv5KRYsKxoCiVQQAvD_BwE"
+                    target="_blank"
                     rel="noopener noreferrer"
                 >
                     <strong>Saiba mais</strong>
@@ -25,20 +25,25 @@ export default function Home() {
             </div>
 
             <div className="cards">
-                <div className="assistente-virtual">
-                    <Image src={Calculadora} width={500} height={300} alt="Calculadora representando serviços de seguros" priority/>
-                    <p>Saiba mais sobre os nossos <br /> serviços</p>
-                    <Link className='btn_click' href="/saiba">Clique aqui</Link>
+                <div className="cards_1">
+                    <div className="assistente-virtual">
+                        <Image src={Calculadora} width={500} height={300} alt="Calculadora representando serviços de seguros" priority />
+                        <p>Saiba mais sobre os nossos <br /> serviços</p>
+                        <Link className='btn_click' href="/saiba">Clique aqui</Link>
+                    </div>
                 </div>
-
-                <div className="medidas-preventivas">
-                    <Image src={MulherMotor} width={500} height={300} alt="Mulher mexendo no motor de um carro" priority />
-                    <p>Medidas preventivas</p>
-                    <Link className='btn_click' href="/medidas">Clique aqui</Link>
+                <div className='cards_2'>
+                    <div className="medidas-preventivas">
+                        <Image src={MulherMotor} width={500} height={300} alt="Mulher mexendo no motor de um carro" priority />
+                        <p>Medidas preventivas</p>
+                        <Link className='btn_click' href="/medidas">Clique aqui</Link>
+                    </div>
                 </div>
             </div>
 
-            
+
+
+
         </div>
     );
 }
